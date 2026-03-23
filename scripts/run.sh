07a79122d6b83d8ef969c96c4842d03dfbaed6e2
Viky Bukovská <xbukovska@stuba.sk>
2026-03-23 12:25:02 +0100

    mongodb

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 COMMAND=${1:-"start"}
 
@@ -6,16 +6,34 @@ PROJECT_ROOT="$(cd "$(dirname "$0")/.." && pwd)"
 
 export AMBULANCE_API_ENVIRONMENT="Development"
 export AMBULANCE_API_PORT="8080"
+export AMBULANCE_API_MONGODB_USERNAME="root"
+export AMBULANCE_API_MONGODB_PASSWORD="neUhaDnes"
+
+mongo() {
+    docker compose --file "${PROJECT_ROOT}/deployments/docker-compose/compose.yaml" "$@"
+}
+
+cleanup() {
+    mongo down
+}
 
 case "$COMMAND" in
-    "start")
+    start)
+        trap cleanup EXIT
+        mongo up --detach
         go run "${PROJECT_ROOT}/cmd/ambulance-api-service"
         ;;
-    "openapi")
-        docker run --rm -it -v "${PROJECT_ROOT}:/local" openapitools/openapi-generator-cli generate -c /local/scripts/generator-cfg.yaml
+    openapi)
+        docker run --rm -ti \
+            -v "${PROJECT_ROOT}:/local" \
+            openapitools/openapi-generator-cli \
+            generate -c /local/scripts/generator-cfg.yaml
+        ;;
+    mongo)
+        mongo up
         ;;
     *)
-        echo "Unknown command: $COMMAND"
+        echo "Unknown command: $COMMAND" >&2
         exit 1
         ;;
 esac
